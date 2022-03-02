1166ff71ae1d6a98b2e6a662bc848a8c892c1a82
dante.panella.contractor.gdevtools.com <dante.panella.contractor@gdevtools.com>
2022-03-02 20:23:53 -0300

    chang enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  authURL: 'https://app-portfolio-backend-1.herokuapp.com/auth/',
+  personalInformationURL: 'https://app-portfolio-backend-1.herokuapp.com/personalInformation/',
+  skillURL: 'https://app-portfolio-backend-1.herokuapp.com/skill/',
+  experienceURL: 'https://app-portfolio-backend-1.herokuapp.com/workExperience/',
+  educationURL: 'https://app-portfolio-backend-1.herokuapp.com/education/',
+  heroURL: 'https://app-portfolio-backend-1.herokuapp.com/hero/',
+  socialURL: 'https://app-portfolio-backend-1.herokuapp.com/social/',
+  projectURL: 'https://app-portfolio-backend-1.herokuapp.com/project/',
+  imageURL: 'https://app-portfolio-backend-1.herokuapp.com/cloudinary/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true, 
+  production: false, 
   authURL: 'https://app-portfolio-backend-1.herokuapp.com/auth/',
   personalInformationURL: 'https://app-portfolio-backend-1.herokuapp.com/personalInformation/',
   skillURL: 'https://app-portfolio-backend-1.herokuapp.com/skill/',
