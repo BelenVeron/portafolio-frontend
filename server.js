1765b49395cc9c3906c48854478bf4235192ba30
dante.panella.contractor.gdevtools.com <dante.panella.contractor@gdevtools.com>
2022-03-03 10:58:09 -0300

    fix server.js

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-portafolio-frontend'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-portafolio-frontend/'}),
+// Static files
+app.use(
+    express.static(path.join(__dirname, '../client/dist/angular-portafolio-frontend'), {
+        maxAge: '1y',
+    })
 );
+// Angular app
+app.get('*', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, '../client/dist/angular-portafolio-frontend/index.html')
+    );
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
